5d7edf2e01913ab44b4be713e15f932a73980860
macbook <macbook@Zohreh.local>
2024-04-09 21:20:31 +0330

    first commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,37 @@
+'use client';
+import React from 'react'
+import Link from 'next/link'
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
+const NavBar = () => {
+  const currenPath = usePathname()
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issue' },
+
+  ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map(link =>
+          <Link key={link.href} className={
+            classNames({
+              'text-zinc-900': link.href === currenPath,
+              'text-zinc-500': link.href !== currenPath,
+              'hover:text-zinc-800 transition-colors': true
+            })
+          }
+
+            href={link.href}>{link.label}</Link>
+
+        )}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ app/issue/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Issues = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Issues
